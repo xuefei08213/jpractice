e761021c17455b19fe18249cf47fd1d642fd9224
jenkins <jenkins@gmail.com>
2018-09-13 09:20:38 +0800

    leetcode 876

---
+++ jpractice-algorithm/src/main/java/org/jpractice/algorithm/leetcode876/ListNode.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.jpractice.algorithm.leetcode876;
+
+/**
+ * @author: 作者： xuefei
+ * @date: 创建时间：2018-09-11 21:59:40
+ * @Description: TODO
+ * @version V1.0
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+}
---
+++ jpractice-algorithm/src/main/java/org/jpractice/algorithm/leetcode876/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.jpractice.algorithm.leetcode876;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: 作者： xuefei
+ * @date: 创建时间：2018-09-11 21:59:14
+ * @Description: TODO
+ * @version V1.0
+ */
+public class Solution {
+
+    public ListNode middleNode(ListNode head) {
+
+        Map<Integer, ListNode> map = new HashMap<>();
+
+        int count = 1;
+        map.put(1, head);
+        while (head.next != null) {
+            count++;
+            map.put(count, head.next);
+            head = head.next;
+        }
+        return map.get((count / 2) + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(5 / 2);
+        System.out.println(5 % 2);
+    }
+
+}
