cc2b4e9f7766dbaa6ceaaeac975c93c58bd46745
jenkins <jenkins@gmail.com>
2018-10-11 08:42:51 +0800

    add image folder

--- jpractice-algorithm/src/main/java/org/jpractice/algorithm/linkedlist/Solution206.java
+++ jpractice-algorithm/src/main/java/org/jpractice/algorithm/linkedlist/Solution206.java
@@ -1,5 +1,34 @@
 package org.jpractice.algorithm.linkedlist;
 
 public class Solution206 {
+    public ListNode reverseList(ListNode head) {
 
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+
+        ListNode node2 = new ListNode(2);
+
+        ListNode node3 = new ListNode(3);
+
+        ListNode node4 = new ListNode(4);
+
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        Solution206 solution206 = new Solution206();
+        solution206.reverseList(head);
+
+    }
 }
